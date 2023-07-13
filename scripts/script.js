d2a3620b08c80fff18057c383cd6145ef290b354
Ginaldo-Silva <138523497+Ginaldo-Silva@users.noreply.github.com>
2023-07-12 22:08:38 -0300

    Testes novos.

---
+++ scripts/script.js
@@ -0,0 +1,22 @@
+function togglePasswordVisibility() {
+
+/*  var imgElement =  document.getElementById('passwordImage');
+  var currentImage = imgElement.getAttribute('src');
+  var newImage =  '';
+
+  if(currentImage === 'img/EyeClossed.png')
+  {
+    newImage = 'img/EyeOpen.png';
+  } else {
+    newImage = 'img/EyeClossed.png';
+  }
+
+  imgElement.setAttribute('src', newImage);
+*/
+  var passwordInput = document.getElementById("ipass");
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+  } else {
+    passwordInput.type = "password";
+  }
+}
